276077fe236dd5892a6e8427f7f09e6acd9187a7
curso.ads <curso.ads@LAB06-75>
2022-07-06 19:26:28 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio4_djalma/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4_djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador do 1 ao 50");
+
+            int num1, num2;
+
+            num1 = 1;
+            num2 = 50;
+
+            for (; num1 <= num2; num1++)
+            {
+                Console.WriteLine(num1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
